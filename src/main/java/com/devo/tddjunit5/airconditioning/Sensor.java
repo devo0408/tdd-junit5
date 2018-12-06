4925c643a55ed7bde451657020b09ebabfef45f5
vovkd <Denys.Vovk@genios.de>
2018-12-06 17:48:56 +0200

    model

---
+++ src/main/java/com/devo/tddjunit5/airconditioning/Sensor.java
@@ -0,0 +1,15 @@
+package com.devo.tddjunit5.airconditioning;
+
+public class Sensor {
+
+    private boolean blocked;
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+}
